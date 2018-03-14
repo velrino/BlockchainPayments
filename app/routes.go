d0992c10850d00bec7a8eb94b6cfa4f706823cb2
velrino <denismagalhaesvelrino@gmail.com>
2018-03-14 07:35:18 -0300

    CORS

--- app/routes.go
+++ app/routes.go
@@ -2,29 +2,33 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	. "github.com/velrino/BlockchainPayments/app/controllers"
 )
 
 func Routes()  {
 	router := gin.Default()
 
+	router.Use(cors.Default())
+
 	router.Static("/doc", "./doc")
-
-	Wallet := router.Group("/wallet")
-	{
-		Wallet.GET("/:coin", TransactionController{}.Wallet)
-	}
-
+	
 	CryptoCurrency := router.Group("/coin")
 	{
-		CryptoCurrency.GET("/:coin",  InfoController{}.Get)
-		CryptoCurrency.GET("/:coin/:value",  InfoController{}.Calculate)
+		CryptoCurrency.GET("/:coin",  CryptoController{}.Get)
+		CryptoCurrency.GET("/:coin/:value",  CryptoController{}.Calculate)
 	}
 
 	Transaction := router.Group("/transaction")
 	{
 		Transaction.GET("/:id",  TransactionController{}.Get)
 		Transaction.POST("/",  TransactionController{}.Create)
+	}
+
+	Wallet := router.Group("/wallet")
+	{
+		Wallet.GET("/", WalletController{}.Address)
+		Wallet.GET("/:address", WalletController{}.Transaction)
 	}
 
 	router.Run(":80")
